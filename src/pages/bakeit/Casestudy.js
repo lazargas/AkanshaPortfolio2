296bfec1ec22d541ca8f7ba9ae043b35b4cdaa06
lazargas <82470472+lazargas@users.noreply.github.com>
2023-04-10 04:04:17 +0530

    font change

--- src/pages/bakeit/Casestudy.js
+++ src/pages/bakeit/Casestudy.js
@@ -1,12 +1,16 @@
 import React from "react";
 import {motion} from "framer-motion";
+import Link from "next/link";
 
 const Casestudy = () => {
   return (
     <motion.div initial={{opacity:0}} animate={{opacity:1}} transition={{duration:0.75, ease:"easeOut"}} >
-      <div className="flex px-14 xl:px-40 justify-between tracking-[5px] font-grand text-4xl pt-10 bg-[#888787]">
+      <div className="flex px-14 xl:px-40 justify-between tracking-[5px] font-tex text-4xl pt-10 bg-[#888787]">
         <div className="relative top-3 ">BAKEit!</div>
+        <Link href="/" >
         <img src="/assets/phoneContactlogo.png" className=" " alt="" />
+        </Link>
+        
       </div>
       <div className="h-screen w-full flex justify-center  bg-[#888787]">
         <embed
--- src/pages/cyclebecho/Casestudy.js
+++ src/pages/cyclebecho/Casestudy.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import {motion} from "framer-motion";
+import Link from 'next/link';
 
 
 const Casestudy = () => {
   return (
     <>
-    <div className='flex px-14 xl:px-40 justify-between font-grand text-4xl pt-10 bg-[#F1D37D]' >
+    <div className='flex px-14 xl:px-40 justify-between font-tex sm:text-xl text-4xl pt-10 bg-[#F1D37D]' >
       <div className='relative top-3 ' >CYCLEBECHO.COM</div>
+      <Link href="/" >
       <img src="/assets/phoneContactlogo.png" className=' ' alt="" />
+      </Link>
+      
     </div>
     <div className='h-screen w-full flex justify-center  bg-[#F1D37D]' >
          
