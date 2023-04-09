b80629fca9775f0760bc3d0211a16f8eb8097438
lazargas <82470472+lazargas@users.noreply.github.com>
2023-04-09 12:32:27 +0530

    font change

--- src/components/atoms/VerticalText/VerticalText.jsx
+++ src/components/atoms/VerticalText/VerticalText.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import style from "./VertixalText.module.css"
+import {motion,useScroll} from "framer-motion"
 
 const VerticalText = ({text}) => {
+  const {scrollProgress} = useScroll();
   return (
-    <p className={style.verticaltext}  >
+    <motion.div 
+    initial={{ opacity: 0,
+      color:"#fff",
+    }}
+  whileInView={{ opacity: 1,
+  color:scrollProgress
+  }}
+    >
+     <p className={style.verticaltext}  >
     {text}
     </p>
+    </motion.div>
+   
   )
 }
 
